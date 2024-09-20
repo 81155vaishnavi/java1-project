89b38481d400d6629cd548b87cff0c0e5e4923a0
admin <admin@DESKTOP-IDFE2KR>
2024-09-20 21:15:25 +0530

    by ABC

---
+++ RedBus/src/main/java/com/cjc/RedBus/RedBusApplication.java
@@ -0,0 +1,14 @@
+package com.cjc.RedBus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedBusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RedBusApplication.class, args);
+		System.out.println("main method added");
+	}
+
+}
